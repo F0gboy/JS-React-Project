b121e281b13dfb77bb5e63c423466e738d340551
Jasper Fog Pedersen <Jasp3r@live.dk>
2025-04-22 17:41:26 +0200

    header med username

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import Header from './Components/Header';
 
 function App() {
+  const [username, setUsername] = useState('');
+
   return (
     <Router>
-      
-      <nav style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+      <Header username={username} />
+
+      <nav style={{ display: 'flex', gap: '1rem', padding: '1rem', background: '#eee' }}>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setUsername={setUsername} />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
